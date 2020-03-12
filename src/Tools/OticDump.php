b9b3cfd96ec3c75a49526656bdec144a3af0a4a8
Matthias <m@tth.es>
2020-03-12 21:28:32 +0100

    added OticDump

---
+++ src/Tools/OticDump.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthias
+ * Date: 12.03.20
+ * Time: 21:23
+ */
+
+namespace OticTools\Tools;
+
+
+use Otic\OticReader;
+
+class OticDump
+{
+
+
+    public static function Dump(string $data, &$stats) : array
+    {
+        $tmpFile = phore_tempfile();
+        $tmpFile->set_contents($data);
+
+        $reader = new OticReader();
+        $reader->open((string)$tmpFile);
+        $stats = [
+            "records" => 0,
+            "first_ts" => null,
+            "last_ts" => null,
+        ];
+
+        $data = [];
+        $reader->setOnDataCallback(function ($timestamp, $colname, $unit, $value) use (&$data, $skipOut) {
+            $data[] = [
+                "ts" => $timestamp,
+                "signal_name" => $colname,
+                "mu" => $unit,
+                "val" => $value
+            ];
+        });
+        $reader->read();
+        return $data;
+    }
+
+
+
+}
